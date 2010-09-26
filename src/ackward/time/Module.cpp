bca464826487a3c4127b00e342836dc513382967
Austin Bingham <austin.bingham@gmail.com>
2010-09-26 20:56:17 +0200

    updated time module to use ackward to generate time.time()

--- src/ackward/time/Module.cpp
+++ src/ackward/time/Module.cpp
@@ -3,17 +3,13 @@
 #include <boost/python/extract.hpp>
 #include <boost/python/import.hpp>
 
+#include <ackward/core/Exceptions.hpp>
+
 using namespace boost::python;
 
 namespace ackward { namespace time
 {
 
 #include <ackward/time/Module_akw.ipp>
-
-float time()
-{
-    return extract<float>(
-        module().attr("time")());
-}
 
 }}
--- src/ackward/time/Module.hpp
+++ src/ackward/time/Module.hpp
@@ -8,8 +8,6 @@ namespace ackward { namespace time
 
 #include <ackward/time/Module_akw.hpp>
 
-float time();
-
 }}
 
 #endif
