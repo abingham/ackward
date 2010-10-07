6ba26abd64cd4a23b994ea4bc54f67aade7202b8
Austin Bingham <austin.bingham@gmail.com>
2010-10-07 18:31:22 +0300

    added UUID::hex()

--- src/test/uuid_test.cpp
+++ src/test/uuid_test.cpp
@@ -1,7 +1,10 @@
+#include <boost/foreach.hpp>
 #include <boost/test/unit_test.hpp>
 
 #include <ackward/core/Exceptions.hpp>
 #include <ackward/uuid/UUID.hpp>
+
+#include <iostream>
 
 using namespace ackward::core;
 using namespace ackward::uuid;
@@ -10,9 +13,16 @@ BOOST_AUTO_TEST_SUITE( UUID_methods )
 
 BOOST_AUTO_TEST_CASE( uuid_fromHex )
 {
-    UUID uuid = UUID::fromHex("{12345678-1234-5678-1234-567812345678}");
-    uuid = UUID::fromHex("12345678123456781234567812345678");
-    uuid = UUID::fromHex("urn:uuid:12345678-1234-5678-1234-567812345678");
+    std::vector<std::string> values;
+    values.push_back("{12345678-1234-5678-1234-567812345678}");
+    values.push_back("12345678123456781234567812345678");
+    values.push_back("urn:uuid:12345678-1234-5678-1234-567812345678");
+    
+    BOOST_FOREACH(const std::string& value, values)
+    {
+        UUID uuid = UUID::fromHex(value);
+        BOOST_CHECK(uuid.hex() == "12345678123456781234567812345678");
+    }
 
     BOOST_CHECK_THROW(
         UUID::fromHex("hello there"),
