bee729ecb7bce0188c7f149a78c216f47e406080
Austin Bingham <austin.bingham@gmail.com>
2010-11-20 10:12:07 +0100

    added missing file

---
+++ src/ackward/logging/Filter.py
@@ -0,0 +1,38 @@
+from ackward import *
+
+class Filter(ClassTranslationUnit):
+    def module(self):
+        return 'logging'
+
+    def preprocessor_guard(self):
+        return 'INCLUDE_ACKWARD_LOGGING_FILTER'
+
+    def header_includes(self):
+        return [
+            ('string',)
+            ]
+
+    def impl_includes(self):
+        includes = [
+            ('ackward', 'logging', 'Filter.hpp'),
+            ('boost', 'python', 'import.hpp'),
+            ]
+
+        return super(Filter, self).impl_includes() + includes
+
+    def forward_declarations(self):
+        return []
+
+    def objects(self):
+        c = Class(name='Filter',
+                  wrapped_class='logging.Filter')
+        
+        Constructor(
+            cls=c,
+            signature=[('std::wstring', 'name')])
+
+        return {
+            ('ackward', 'logging') : [c]}
+
+def definition():
+    return Filter()
