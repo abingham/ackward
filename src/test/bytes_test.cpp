922b3b1fe76c4ea654511c68931efc13f843037c
Austin Bingham <austin.bingham@gmail.com>
2011-01-02 16:55:13 +0100

    more Bytes tests

--- src/test/bytes_test.cpp
+++ src/test/bytes_test.cpp
@@ -5,6 +5,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include <ackward/core/Bytes.hpp>
+#include <ackward/core/Exceptions.hpp>
 
 using namespace ackward::core;
 namespace bp=boost::python;
@@ -25,10 +26,47 @@ BOOST_AUTO_TEST_CASE( existing_ctor )
     BOOST_CHECK(std::string(b.begin(), b.end()) == data);
 }
 
+BOOST_AUTO_TEST_CASE( existing_ctor_throws_value_error )
+{
+    BOOST_CHECK_THROW(
+        Bytes(bp::object(4)),
+        ackward::core::ValueError);
+}
+
+BOOST_AUTO_TEST_CASE( from_data_ctor )
+{
+    std::string data("asdf");
+
+    Bytes b(data.c_str(), data.size());
+    BOOST_CHECK((std::size_t)b.size() == data.size());
+    BOOST_CHECK(std::string(b.begin(), b.end()) == data);
+}
+
 BOOST_AUTO_TEST_CASE( default_ctor )
 {
     Bytes b;
     BOOST_CHECK(b.size() == 0);
+}
+
+BOOST_AUTO_TEST_CASE( index )
+{
+    std::string data("1234");
+    Bytes b(data.c_str(), data.size());
+    
+    for (int i = 0; i < b.size(); ++i)
+    {
+        BOOST_CHECK(data[i] == b[i]);
+    }
+}
+
+BOOST_AUTO_TEST_CASE( index_throw )
+{
+    std::string data("1234");
+    Bytes b(data.c_str(), data.size());
+
+    BOOST_CHECK_THROW(
+        b[5],
+        IndexError);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
