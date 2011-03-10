b681d33d7d0200f3e2a353a901103d7a6732fd69
Austin Bingham <austin.bingham@gmail.com>
2011-03-10 22:10:24 +0100

    added namespace construct

---
+++ site_scons/ackward/namespace.py
@@ -0,0 +1,23 @@
+from .element import Element
+
+def to_open(ns):
+    return ' '.join(['namespace {0} {'.format(n) for n in ns])
+
+def to_close(ns):
+    return '}' * len(ns)
+
+class Namespace(Element):
+    def __init__(self, *args):
+        self.ns = list(args)
+
+    def open_header(self, mod, symbols):
+        yield to_open(self.ns)
+
+    def close_header(self, mod, symbols):
+        yield to_close(self.ns)
+
+    def open_impl(self, mod, symbols):
+        yield to_open(self.ns)
+
+    def close_impl(self, mod, symbols):
+        yield to_close(self.ns)
