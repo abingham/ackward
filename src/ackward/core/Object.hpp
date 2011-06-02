c57c9028e39e19ed9b5dab625ab3e6ab83b0e6ed
Austin Bingham <austin.bingham@gmail.com>
2011-06-02 13:33:57 +0200

    added docstrings to core::Object et al.

--- src/ackward/core/Object.hpp
+++ src/ackward/core/Object.hpp
@@ -21,7 +21,12 @@ class Object
 public:
     Object(boost::python::object);
 
+    /** Get the underlying boost::python::object.
+     */
     boost::python::object obj();
+
+    /** Get the underlying boost::python::object.
+     */
     const boost::python::object obj() const;
 
 private:
@@ -30,12 +35,43 @@ private:
 
 }}
 
+/** \rst
+    
+    Define an interface to an attribute on the underlying Python
+    object.
+
+    This adds a member function to a core::Object that returns the
+    value of an attribute on the Python object wrapped by a
+    core::Object. The value of the underlying attribute is converted
+    to a C++ type using ``boost::python::extract`` to ``result_type``.
+
+    Args:
+      * result_type: The type of the attribute in C++.
+      * name: The name of the attribute on the object.
+
+    \endrst
+ */
 #define ACKWARD_ATTR(result_type, name)               \
     result_type name() const {                        \
         return boost::python::extract<result_type>(   \
             obj().attr(#name));                       \
     }
 
+/** \rst
+
+    Define an interface to a 0-arity method on the underlying Python
+    object.
+
+    This is just like ACKWARD_ATTR except that this calls the
+    underlying attribute and return the resulting value rather than
+    just returning the attribute itself.
+
+    Args:
+      * result_type: The type of the attribute in C++.
+      * name: The name of the attribute on the object.
+
+    \endrst
+ */
 #define ACKWARD_METHOD(result_type, name)       \
     result_type name() const {                  \
     return boost::python::extract<result_type>( \
