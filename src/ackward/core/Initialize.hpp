ce1fa0014c428e5beb32cf4564b1345e6bb7e767
Austin Bingham <austin.bingham@gmail.com>
2011-06-02 11:51:48 +0200

    added a docstring to core::initialize()

--- src/ackward/core/Initialize.hpp
+++ src/ackward/core/Initialize.hpp
@@ -4,6 +4,11 @@
 namespace ackward {
 namespace core {
 
+/** Initialize the ackward::core module.
+
+    This should be called before using any other parts of
+    ackward::core.
+ */
 void initialize();
 
 }
