7bd0795604eb7a6f5c09ca463f72f39365eec051
Austin Bingham <austin.bingham@gmail.com>
2011-06-05 18:28:06 +0200

    added some documentation for the PythonVersion macros.

--- src/ackward/core/PythonVersion.hpp
+++ src/ackward/core/PythonVersion.hpp
@@ -3,8 +3,13 @@
 
 #include <Python.h>
 
+/** The Python major version number, e.g. 2 or 3 */
 #define ACKWARD_PYTHON_MAJOR_VERSION PY_MAJOR_VERSION
+
+/** The Python minor version number, e.g. 6 or 3 */
 #define ACKWARD_PYTHON_MINOR_VERSION PY_MINOR_VERSION
+
+/** The full Python version number */
 #define ACKWARD_PYTHON_VERSION PY_VERSION
 
 #endif
