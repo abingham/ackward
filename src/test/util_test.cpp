295e09be4f2f65f1aa20b837431066ae368a3e0b
Austin Bingham <austin.bingham@gmail.com>
2011-06-08 20:52:54 +0200

    Added more tests for the core utils.

--- src/test/util_test.cpp
+++ src/test/util_test.cpp
@@ -1,6 +1,7 @@
 #include <boost/python.hpp>
 #include <boost/test/unit_test.hpp>
 
+#include <ackward/core/Exceptions.hpp>
 #include <ackward/core/Import.hpp>
 #include <ackward/core/Util.hpp>
 
@@ -27,6 +28,75 @@ BOOST_AUTO_TEST_CASE( verifyType_test )
     verifyType(foo, "str");
     BOOST_CHECK_THROW(verifyType(foo, "int"), ValueError);
     BOOST_CHECK_THROW(verifyType<TypeError>(foo, "int"), TypeError);
+}
+
+BOOST_AUTO_TEST_CASE( is_none_test )
+{
+    BOOST_CHECK( is_none(builtins().attr("None")) );
+    BOOST_CHECK( !is_none(bp::object(1)) );
+    BOOST_CHECK( !is_none(bp::object("asdf")) );
+}
+
+BOOST_AUTO_TEST_CASE( iter_test )
+{
+    bp::list l;
+    l.append(1);
+    l.append(2);
+    
+    // iter() creates a usable iterator
+    bp::object i = iter(l);
+    BOOST_CHECK( bp::extract<int>(next(i)) == 1);
+    BOOST_CHECK( bp::extract<int>(next(i)) == 2);
+    
+    BOOST_CHECK_THROW(
+        next(i),
+        StopIteration);
+
+    // Throw for non-iterables
+    BOOST_CHECK_THROW(
+        iter(bp::object(5)),
+        TypeError);
+}
+
+BOOST_AUTO_TEST_CASE( next_test )
+{
+    bp::list l;
+    l.append(1);
+    l.append(2);
+    
+    // iter() creates a usable iterator
+    bp::object i = iter(l);
+    BOOST_CHECK( bp::extract<int>(next(i)) == 1);
+    BOOST_CHECK( bp::extract<int>(next(i)) == 2);
+    
+    BOOST_CHECK_THROW(
+        next(i),
+        StopIteration);
+
+    bp::object o(5);
+    BOOST_CHECK_THROW(
+        next(o),
+        TypeError);
+}
+
+BOOST_AUTO_TEST_CASE( strToWString_test )
+{
+    bp::str s("åøæ");
+    std::wstring ws =
+        strToWString(s);
+    BOOST_CHECK( ws == std::wstring(L"åøæ") );
+}
+
+BOOST_AUTO_TEST_CASE( fromPythonConvertible_test )
+{
+    BOOST_CHECK(
+        fromPythonConvertible<int>(
+            boost::python::object(5).ptr()));
+
+    BOOST_CHECK(
+        !fromPythonConvertible<int>(
+            boost::python::object("Magnited we stand.").ptr()));
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
