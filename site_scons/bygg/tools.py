5057eee74b83a78200806cc61794a3e9eb00fb39
Austin Bingham <austin.bingham@gmail.com>
2011-10-30 09:11:15 +0100

    Added support to bygg for letting build products express their own dependencies.

--- site_scons/bygg/tools.py
+++ site_scons/bygg/tools.py
@@ -1,22 +1,32 @@
 import os
 
+from .build_products import BuildProducts as products
+
 class SharedLib(object):
     '''Represents a single shared library in the build_products map.
 
     This can be used to configure build environments to link/build
     against this library.
     '''
-    def __init__(self, lib_name, lib_dir, include_dir):
+    def __init__(self,
+                 lib_name,
+                 lib_dir,
+                 include_dir,
+                 deps=[]):
         self.lib_name = lib_name
         self.lib_dir = lib_dir
         self.include_dir = include_dir
-        
+        self.deps = deps
+
     def configure(self, env, rpath=False):
         env.AppendUnique(LIBS=[self.lib_name])
         env.AppendUnique(LIBPATH=[self.lib_dir])
         env.AppendUnique(CPPPATH=[self.include_dir])
-        #if rpath:
-        #    env.AppendUnique(RPATH=[os.path.split(self.node.path)[0]])
+
+        print(self.lib_name, self.deps)
+
+        for dep in self.deps:
+            products(env)[dep].configure(env)
 
 def _install_headers(env, headers, include_dir):
     for header in headers:
@@ -39,11 +49,10 @@ def build_shared_library(env,
                          lib_name=None):
     '''A central rule for building shared libraries.
     '''
-    from . import products
 
     if not lib_name:
         lib_name = name
-    
+
     local_env = env.Clone()
     lib = local_env.SharedLibrary(lib_name, sources)
     for dep in deps:
@@ -52,7 +61,8 @@ def build_shared_library(env,
     products(env)[name] = SharedLib(
         lib_name,
         lib_dir,
-        include_dir)
+        include_dir,
+        deps=deps)
 
     env.Alias('build', lib)
     env.Alias(
@@ -71,8 +81,6 @@ def build_program(env,
                   sources,
                   bin_dir,
                   deps=[]):
-    from . import products
-    
     local_env = env.Clone()
     bin = local_env.Program(name, sources)
     for dep in deps:
