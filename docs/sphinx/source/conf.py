97db9cf90fac60b2799a77f5a025eb96693d4c77
Austin Bingham <austin.bingham@gmail.com>
2012-04-15 17:29:12 +0200

    updated copyright dates.

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ackward'
-copyright = u'2011, Austin Bingham'
+copyright = u'2011,2012 Austin Bingham'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
